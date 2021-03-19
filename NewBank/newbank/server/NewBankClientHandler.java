21e44c983cf13bc654813cb58a258ad9f67ca0d5
opciok <50515248+opciok@users.noreply.github.com>
2021-03-19 15:22:56 +0000

    Adding CommandParser class
    - class parses the command in the form param: paramValue
    - if there is no paramValue it assigns NULL
    - returns empty map if the parameter is not in a form "-x"

--- NewBank/newbank/server/NewBankClientHandler.java
+++ NewBank/newbank/server/NewBankClientHandler.java
@@ -5,62 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
-public class NewBankClientHandler extends Thread{
+public class NewBankClientHandler extends Thread {
 
-	//added a socket s to send out requests for more data to customers
-	private NewBank bank;
-	private BufferedReader in;
-	private PrintWriter out;
-	private Socket s;
+  //added a socket s to send out requests for more data to customers
+  private NewBank bank;
+  private BufferedReader in;
+  private PrintWriter out;
+  private Socket s;
 
 
-	public NewBankClientHandler(Socket s) throws IOException {
-		this.s = s;
-		bank = NewBank.getBank();
-		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		out = new PrintWriter(s.getOutputStream(), true);
-	}
-	
-	public void run() {
-		// keep getting requests from the client and processing them
-		try {
-			// ask for user name
-			out.println("Enter Username");
-			String userName = in.readLine();
-			// ask for password
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// if the user is authenticated then get requests from the user and process them 
-			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
-				while(true) {
-					String request = in.readLine();
-					System.out.println("Request from " + customer.getKey());
-					String responce = bank.processRequest(customer, request, s);
-					out.println(responce);
-					//added this so exiting doesn't look as though nothing is happening
-					out.println("What do you want to do?");
-				}
-			}
-			else {
-				out.println("Log In Failed");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				in.close();
-				out.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-				Thread.currentThread().interrupt();
-			}
-		}
-	}
+  public NewBankClientHandler(Socket s) throws IOException {
+    this.s = s;
+    bank = NewBank.getBank();
+    in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    out = new PrintWriter(s.getOutputStream(), true);
+  }
+
+  public void run() {
+    // keep getting requests from the client and processing them
+    try {
+      // ask for user name
+      out.println("Enter Username");
+      String userName = in.readLine();
+      // ask for password
+      out.println("Enter Password");
+      String password = in.readLine();
+      out.println("Checking Details...");
+      // authenticate user and get customer ID token from bank for use in subsequent requests
+      CustomerID customer = bank.checkLogInDetails(userName, password);
+      // if the user is authenticated then get requests from the user and process them
+      if (customer != null) {
+        out.println("Log In Successful. What do you want to do?");
+        while (true) {
+          String request = in.readLine();
+          System.out.println("Request from " + customer.getKey());
+          String responce = bank.processRequest(customer, request, s);
+          out.println(responce);
+          //added this so exiting doesn't look as though nothing is happening
+          out.println("What do you want to do?");
+        }
+      } else {
+        out.println("Log In Failed");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        in.close();
+        out.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
 
 }
