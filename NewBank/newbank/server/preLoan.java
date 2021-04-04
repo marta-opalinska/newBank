0a797503baf62caab8f952b0c50dbaa6fdad3dc9
Rasmus <80233727+rasmus9889@users.noreply.github.com>
2021-04-04 21:48:54 +0200

    added preloan abstract class
    new class that can be used to build loanoffers and loanrequests
    uses constructor of a customer class(the creator), initial amount, annual APR(in percent) and length of loan in days

--- NewBank/newbank/server/preLoan.java
+++ NewBank/newbank/server/preLoan.java
@@ -2,7 +2,31 @@ package newbank.server;
 
 public abstract class preLoan {
     status loanStatus;
-    
+    Customer creator;
+    double amount;
+    double repaymentAmount;
+    int daysToRepayment;
+
+    public preLoan(Customer cust, double initialAmount, double annualAPR, int days){
+        this.creator = cust;
+        this.amount = initialAmount;
+        this.daysToRepayment = days;
+        this.repaymentAmount = getRepaymentAmount(initialAmount, annualAPR, days);
+    }
+
+    public boolean changeStatus(status newStatus){
+        try{
+            this.loanStatus = newStatus;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public double getRepaymentAmount(double Amount, double annualAPR, int days){
+        double effectiveAPR = Math.pow(((annualAPR/100)+1), days/365);
+        return Amount*effectiveAPR;
+    }
 
 }
 enum status{
